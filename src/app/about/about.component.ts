99fa098ed86bcd061b2921139181f4739d4d615f
travisb1210 <travisb1210@gmail.com>
2024-03-26 12:08:18 -0500

    changing views

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,16 +6,20 @@ import { Component, Input } from '@angular/core';
   imports: [],
   template: `
   <div class="about-me">
-    <div class="about-me-content">    
-      <img src="assets/travis.jpeg" alt="Travis Broussard photo">
-    </div>
-    <div class="about-me-content">
-      <p>
-        Hi my name is Travis Broussard! I am a software developer who's primary experience lies in server-side development. Although
-        my primary experience involves working with Java, I also have had exposure with database related tasks involving SQL and frontend 
-        development utilizing HTML, CSS, and JavaScript. This website was created using these three technologies along with the Angular
-        framework. Please take a look at the skills section to see the full list of technologies that I have experience with.
-      </p>
+    <div class="about-me-content-container">
+        <div class="about-me-content">
+        <div>    
+          <img src="assets/travis.jpeg" alt="Travis Broussard photo">
+        </div>
+        <div>
+          <p>
+            Hi my name is Travis Broussard! I am a software developer who's primary experience lies in server-side development. Although
+            my primary experience involves working with Java, I also have had exposure with database related tasks involving SQL and frontend 
+            development utilizing HTML, CSS, and JavaScript. This website was created using these three technologies along with the Angular
+            framework. Please take a look at the skills section to see the full list of technologies that I have experience with.
+          </p>
+        </div>
+      </div>
     </div>
   </div>
   `,
